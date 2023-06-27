8f336319ae10bc66afece6155fd6f758dc3eda7b
Ion Madalin Grecescu <madalin.grecescu@gmail.com>
2023-06-27 21:18:56 +0300

    First comiit - early stages of the front end

---
+++ src/main/java/com/bookStore/Nook/NookApplication.java
@@ -0,0 +1,13 @@
+package com.bookStore.Nook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookStore/Nook/NookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookStore.Nook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
